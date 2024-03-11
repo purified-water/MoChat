132057184e254f407112c1d27d427d6f6c3ece22
“purified-water” <julianng1110@gmail.com>
2024-03-11 22:11:16 +0700

    chore: Fix when keyboard appears, it pushes the Login form up

---
+++ screens/LoginScreen.js
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, SafeAreaView, TouchableOpacity, Alert, Image, KeyboardAvoidingView } from 'react-native';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../config/firebase';
+const background = require('../assets/purple-surface.png');
+
+function LoginScreen({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        if (email !== '' && password !== '') {
+            signInWithEmailAndPassword(auth, email, password)
+                .then(() => console.log('Login success'))
+                .catch(error => Alert.alert('Error', error.message));
+
+        }
+        else if (email === '' || password === '') {
+            Alert.alert('Error', 'Email and password are required');
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image source={background} style={styles.backImage}></Image>
+            <View style={styles.loginContainer}>
+                
+            </View>
+            <SafeAreaView style={styles.form}>
+                    <Text style={styles.title}>Login</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Enter email'
+                        autoCapitalize='none'
+                        textContentType='emailAddress'
+                        value={email}
+                        onChangeText={(text) => setEmail(text)}>
+                    </TextInput>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Enter password'
+                        autoCapitalize='none'
+                        textContentType='password'
+                        value={password}
+                        onChangeText={(text) => setPassword(text)}>
+
+                    </TextInput>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={{ color: 'white' }} onPress={handleLogin}>Login</Text>
+                    </TouchableOpacity>
+                    <Text>Don't have an account? Click here</Text>
+
+
+                </SafeAreaView>
+        </View>
+
+
+    )
+}
+
+export default LoginScreen;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    backImage: {
+        width: '100%',
+        height: '40%',
+        resizeMode: 'cover',
+        justifyContent: 'center'
+    },
+    loginContainer: {
+        width: '100%',
+        height: '75%',
+        position: 'absolute',
+        bottom: 0,
+        backgroundColor: 'white',
+        borderTopLeftRadius: 70
+    },
+    form: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 200
+    },
+    title: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: 'red',
+        marginBottom: 20
+    },
+    input: {
+        backgroundColor: '#F6F7FB',
+        height: 58,
+        width: 300,
+        marginBottom: 20,
+        fontSize: 16,
+        borderRadius: 10,
+        padding: 10
+    },
+    button: {
+        height: 58,
+        width: 300,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 15,
+        marginBottom: 10,
+        backgroundColor: '#522687',
+        borderRadius: 10
+    }
+});
+
